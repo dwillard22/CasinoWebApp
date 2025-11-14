b3c67145d1906eb574f59246d176f951222b192a
colton-dover <colton.dover@snhu.edu>
2025-11-13 23:34:08 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     allowedHosts: [
-      '75d241a3-e478-46fb-8fc9-7cd65b814d2d-00-3b1582jnqbq4s.picard.replit.dev'
+      "75d241a3-e478-46fb-8fc9-7cd65b814d2d-00-3b1582jnqbq4s.picard.replit.dev"
     ]
   }
 })
