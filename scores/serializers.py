7f2f75e14b8592e125934f680e80d5e722d82f3b
dominic willard <dominicjwillard@gmail.com>
2025-11-14 09:17:28 -0500

    deleted scores folder

--- scores/serializers.py
+++
@@ -1,9 +0,0 @@
-"""serializer for leaderboard model in the casino application."""
-
-from rest_framework import serializers
-from .models import Leaderboard
-
-class LeaderboardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Leaderboard
-        fields = "__all__"
